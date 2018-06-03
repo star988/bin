e8c717760240f7c690b20590516f09452dca09a9
a58625074@126.com <a58625074@126.com>
2018-06-03 16:25:40 +0800

    es6 basic lesson  is end

--- learn_es6/app/js/index.js
+++ learn_es6/app/js/index.js
@@ -1,3 +1,5 @@
+import 'babel-polyfill';
+// import 'babel-plugin-transform-decorators-legacy';
 // import './lesson/lesson1.js';
 // import './lesson/lesson2.js';
 // import './lesson/lesson3.js';
@@ -11,4 +13,22 @@
 // import './lesson/lesson11.js';
 // import './lesson/lesson12.js';
 // import './lesson/lesson13.js';
-import './lesson/lesson14.js';
+// import './lesson/lesson14.js';
+// import './lesson/lesson15.js';
+// import './lesson/lesson16.js';
+
+// import {
+//     a,
+//     Code,
+//     fn
+// } from './lesson/lesson17';
+
+// console.log(a, Code, fn);
+
+// import * as lesson17 from "./lesson/lesson17";
+
+// console.log(lesson17.a, lesson17.fn)
+
+import lesson17 from './lesson/lesson17';
+
+console.log(lesson17.Parent, lesson17.A);
---
+++ learn_es6/app/js/lesson/lesson17.js
@@ -0,0 +1,25 @@
+// export let a = 'test';
+
+// export let fn = () => {
+//     return 'fn'
+// }
+
+// export class Code {
+
+// }
+
+
+let A = 'test';
+
+let fn = function () {
+
+}
+class Parent {
+
+}
+
+export default {
+    A,
+    fn,
+    Parent
+}
